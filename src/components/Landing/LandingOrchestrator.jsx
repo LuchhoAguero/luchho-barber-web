db90b2119bc7aed4b966e75adcbe49c01ca8ca74
luchhoaguero <lucianoaguero1970@gmail.com>
2025-12-10 19:49:16 -0300

    "Fix estructura Orchestrator: titulo dentro de flexbox"

--- src/components/Landing/LandingOrchestrator.jsx
+++ src/components/Landing/LandingOrchestrator.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import styles from "./LandingOrchestrator.module.scss";
 
-// Importamos el video de fondo
 import BackgroundVideo from "../BackgroundVideo/BackgroundVideo";
-// CAMBIO: En vez de BookingCard, importamos la nueva ContactSection
 import ContactSection from "../ContactSection/ContactSection";
 
 const LandingOrchestrator = ({ videoSource, phoneNumber }) => {
   return (
     <section className={styles.orchestratorContainer}>
-      {/* 1. El Video de fondo (Sigue igual) */}
+      {/* 1. El Video de fondo queda igual */}
       <BackgroundVideo videoSource={videoSource} />
-      <h1 className={styles.brandTitle}>
-        LUCHHO<span className={styles.goldText}>BARBER</span>
-      </h1>
-      {/* 2. El contenido central (Ahora es la ContactSection) */}
+
+      {/* 2. EL CAMBIO CLAVE: Metemos todo DENTRO de centerContent */}
       <div className={styles.centerContent}>
+        {/* AQUI va el Título, ahora es hijo del flexbox */}
+        <h1 className={styles.brandTitle}>
+          LUCHHO<span className={styles.goldText}>BARBER</span>
+        </h1>
+
+        {/* Y debajo la sección de contacto */}
         <ContactSection phoneNumber={phoneNumber} />
       </div>
     </section>
